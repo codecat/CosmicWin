73fa67efa27eeaed1b7876f6012fe34d4a945044
duckson <duckson@hyperion>
2012-03-01 16:20:39 +0100

    Moved a.js to a separate file.
    A.js is now be @require'd in the meta block.
    This way, it will download when installing CosmicWin.

--- cosmicwin.meta.js
+++ cosmicwin.meta.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name           CosmicWin
-// @version        1.00
+// @version        1.01
 // @namespace      CosmicWin
 // @description    Makes the new YouTube homepage better.
 // @include        https://*.youtube.com/
 // @include        http://*.youtube.com/
 // @include        https://youtube.com/
 // @include        http://youtube.com/
-// @downloadURL    https://github.com/AngeloG/CosmicWin/raw/master/cosmicwin.user.js
-// @updateURL      https://github.com/AngeloG/CosmicWin/raw/master/cosmicwin.meta.js
+// @require        https://github.com/duckson/CosmicWin/raw/master/a.js
+// @downloadURL    https://github.com/duckson/CosmicWin/raw/master/cosmicwin.user.js
+// @updateURL      https://github.com/duckson/CosmicWin/raw/master/cosmicwin.meta.js
 // ==/UserScript==
