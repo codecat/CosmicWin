54140561ff6da708bc75fa2f64bf5eacc73666bf
duckson <duckson@hyperion>
2012-03-02 15:43:33 +0100

    URL's now point to AngeloG's repository.

--- cosmicwin.user.js
+++ cosmicwin.user.js
@@ -7,9 +7,9 @@
 // @include        http://*.youtube.com/
 // @include        https://youtube.com/
 // @include        http://youtube.com/
-// @require        https://github.com/duckson/CosmicWin/raw/master/a.js
-// @downloadURL    https://github.com/duckson/CosmicWin/raw/master/cosmicwin.user.js
-// @updateURL      https://github.com/duckson/CosmicWin/raw/master/cosmicwin.meta.js
+// @require        https://github.com/AngeloG/CosmicWin/raw/master/a.js
+// @downloadURL    https://github.com/AngeloG/CosmicWin/raw/master/cosmicwin.user.js
+// @updateURL      https://github.com/AngeloG/CosmicWin/raw/master/cosmicwin.meta.js
 // ==/UserScript==
 
 (function() {
